f201f10097b875cd3357f4f33f7aba2056359721
Richard Franks <richard.franks@securenetcoms.co.uk>
2023-07-14 11:15:40 +0100

    Updated to have tier 1 gateway as optional

--- input.tf
+++ input.tf
@@ -7,10 +7,6 @@ variable "segment_description" {
   description = "A description of the segment."
   default     = ""
 }
-variable "edge_cluster_name" {
-  type        = string
-  description = "Name of the edge cluster on which to place the tier 1 router"
-}
 variable "tier0_gateway_name" {
   type        = string
   description = "The name of the tier 0 gateway to which to connect the tier 1 gateway."
@@ -19,21 +15,31 @@ variable "transport_zone_name" {
   type        = string
   description = "Name of the transport zone in which to create the segment."
 }
-variable "route_advertisement_types" {
-  type        = list(string)
-  description = "Enable different types of route advertisements: TIER1_STATIC_ROUTES, TIER1_CONNECTED, TIER1_NAT, TIER1_LB_VIP, TIER1_LB_SNAT, TIER1_DNS_FORWARDER_IP, TIER1_IPSEC_LOCAL_ENDPOINT."
-  default     = ["TIER1_STATIC_ROUTES", "TIER1_CONNECTED"]
-}
 variable "subnets" {
   type        = list(string)
   description = "List of subnets to assign to the segment in CIDR format."
 }
-variable "failover_mode" {
+variable "create_tier1_gateway" {
+  type        = bool
+  description = "Whether to create a tier 1 gateway"
+  default     = true
+}
+variable "tier1_edge_cluster_name" {
+  type        = string
+  description = "Name of the edge cluster on which to place the tier 1 router"
+  default     = null
+}
+variable "tier1_route_advertisement_types" {
+  type        = list(string)
+  description = "Enable different types of route advertisements: TIER1_STATIC_ROUTES, TIER1_CONNECTED, TIER1_NAT, TIER1_LB_VIP, TIER1_LB_SNAT, TIER1_DNS_FORWARDER_IP, TIER1_IPSEC_LOCAL_ENDPOINT."
+  default     = ["TIER1_STATIC_ROUTES", "TIER1_CONNECTED"]
+}
+variable "tier1_failover_mode" {
   type        = string
   description = "Failover mode of the tier 1 gateway."
   default     = "PREEMPTIVE"
 }
-variable "enable_firewall" {
+variable "tier1_enable_firewall" {
   type        = string
   description = "Whether to enable the firewall on the tier 1 gateway."
   default     = "false"
--- terraform.tf
+++ terraform.tf
@@ -12,7 +12,7 @@ data "nsxt_policy_tier0_gateway" "T0" {
 }
 
 data "nsxt_policy_edge_cluster" "EC" {
-  display_name = var.edge_cluster_name
+  display_name = var.tier1_edge_cluster_name
 }
 
 data "nsxt_policy_transport_zone" "tz" {
@@ -20,22 +20,23 @@ data "nsxt_policy_transport_zone" "tz" {
 }
 
 resource "nsxt_policy_tier1_gateway" "tier1_gw" {
+  count                     = var.create_tier1_gateway ? 1 : 0
   description               = "${var.segment_name} router"
   display_name              = "${var.segment_name}-gw1"
   edge_cluster_path         = data.nsxt_policy_edge_cluster.EC.path
-  failover_mode             = var.failover_mode
+  failover_mode             = var.tier1_failover_mode
   default_rule_logging      = "false"
-  enable_firewall           = var.enable_firewall
+  enable_firewall           = var.tier1_enable_firewall
   enable_standby_relocation = "false"
   tier0_path                = data.nsxt_policy_tier0_gateway.T0.path
-  route_advertisement_types = var.route_advertisement_types
+  route_advertisement_types = var.tier1_route_advertisement_types
   pool_allocation           = "ROUTING"
 }
 
 resource "nsxt_policy_segment" "segment" {
   display_name        = var.segment_name
   description         = var.segment_description
-  connectivity_path   = nsxt_policy_tier1_gateway.tier1_gw.path
+  connectivity_path   = var.create_tier1_gateway ? nsxt_policy_tier1_gateway.tier1_gw[0].path : data.nsxt_policy_tier0_gateway.T0.path
   transport_zone_path = data.nsxt_policy_transport_zone.tz.path
 
   dynamic "subnet" {
